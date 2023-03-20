ca1f34f205dbbd1460115fcc13afaaf5daab738e
lupasealtestgit <alin.qutob@outlook.com>
2023-03-20 14:39:50 +0200

    add JSX to variable

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-//ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
+// const newHeader = document.createElement("h1")
+// newHeader.textContent = "Hello, world!"
+// newHeader.className = "header"
+// const rootDiv = document.querySelector("#root")
+// rootDiv.appendChild(newHeader);
 
-const newHeader = document.createElement("h1")
+const page = (
+  <div>
+    <h4 className="header">This is JSX!</h4>
+    <p>test</p>
+  </div>
+)
 
-newHeader.textContent = "Hello, world!"
+ReactDOM.render(page, document.getElementById("root"))
 
-newHeader.className = "header"
-
-const rootDiv = document.querySelector("#root")
-
-rootDiv.appendChild(newHeader);
