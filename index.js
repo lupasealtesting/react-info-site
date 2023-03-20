ff25cd549243424881b59f2b0b7bb926b79d185f
lupasealtestgit <alin.qutob@outlook.com>
2023-03-20 13:11:20 +0200

    Create render with vanilla JS

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-function MainContent() {
-    return (
-    <h4>I'm learning React!</h4>
-    )
-}
+//ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
 
-ReactDOM.render(
-    <div>
-        <MainContent />
-    </div>,
-    document.getElementById("root")
-)
+const newHeader = document.createElement("h1")
+
+newHeader.textContent = "Hello, world!"
+
+newHeader.className = "header"
+
+const rootDiv = document.querySelector("#root")
+
+rootDiv.appendChild(newHeader);
