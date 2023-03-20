048c87a850e675d4e02fc9efb2aa3e4be9ce371e
lupasealtestgit <alin.qutob@outlook.com>
2023-03-20 17:01:18 +0200

    create static react with header/footer and logo

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 
 
 
-
-function TemporaryName() {
-    return (
-
- <div>
-    <img src="./react-logo.png" width="40px"/> 
-      <h1>Fun facts about React</h1>
-        <ul>
-            <li><b>Was first releasd in 2013</b></li>
-            <li><b>Was originally created by Jordan Walke</b></li>
-            <li><b>Has well over 100K stars on Github</b></li>
-            <li><b>Is maintained by Facebook</b></li>
-            <li><b>Powers thousands of enterprise apps, including mobile apps</b></li>
-        </ul>
- </div>
-
+function Page(){
+    return(
+            <div>
+            <header>
+              <nav>
+                <img src="./react-logo.png" width="40px"/>
+                </nav>
+            </header>
+            <h3>
+                Reasons I want React as it is an amazing way to be declarative
+            </h3>
+            <ol>
+                <li>Because it is not difficult</li>
+                <li>Less Time to code</li>
+                <li>Great skill to have</li>
+            </ol>
+            <footer>
+            <small>Ⓒ 2023 LupaSeal development. All rights reserved.</small>
+            </footer>
+            </div>
     )
 }
 
-ReactDOM.render(<TemporaryName />,document.getElementById("root"))
-
+ReactDOM.render(<Page />,document.getElementById("root"))
