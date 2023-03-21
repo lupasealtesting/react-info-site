7baab0ccab58f7cfa7a540a1ba724b3a0e416813
lupasealtestgit <alin.qutob@outlook.com>
2023-03-21 10:11:11 +0200

    Testing 2nd day

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 
 
 
-function Page(){
-    return(
-            <div>
-            <header>
-              <nav>
-                <img src="./react-logo.png" width="40px"/>
-                </nav>
-            </header>
-            <h3>
-                Reasons I want React as it is an amazing way to be declarative
-            </h3>
-            <ol>
-                <li>Because it is not difficult</li>
-                <li>Less Time to code</li>
-                <li>Great skill to have</li>
-            </ol>
-            <footer>
-            <small>Ⓒ 2023 LupaSeal development. All rights reserved.</small>
-            </footer>
-            </div>
-    )
-}
+// function Page(){
+//     return(
+//             <div>
+//             <header>
+//               <nav>
+//                 <img src="./react-logo.png" width="40px"/>
+//                 </nav>
+//             </header>
+//             <h3>
+//                 Reasons I want React as it is an amazing way to be declarative
+//             </h3>
+//             <ol>
+//                 <li>Because it is not difficult</li>
+//                 <li>Less Time to code</li>
+//                 <li>Great skill to have</li>
+//             </ol>
+//             <footer>
+//             <small>Ⓒ 2023 LupaSeal development. All rights reserved.</small>
+//             </footer>
+//             </div>
+//     )
+// }
 
-ReactDOM.render(<Page />,document.getElementById("root"))
+// ReactDOM.render(<Page />,document.getElementById("root"))
+
+1. what is a React component?
+reusable code
+2.child without parent
+3.missing div or empty <></>
