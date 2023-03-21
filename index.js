21bfb3f5d9d55cf89b8bfe1bad935dcf22f4102b
lupasealtestgit <alin.qutob@outlook.com>
2023-03-21 10:45:27 +0200

    Parent/child component

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
 
+function Header(){
+    return (
+    <header>
+    <nav>
+      <img src="./react-logo.png" width="40px"/>
+      </nav>
+  </header>
+    )
+}
 
+function Footer(){
+    return(
+        <footer>
+        <small>Ⓒ 2023 LupaSeal development. All rights reserved.</small>
+        </footer>
+    )
+}
 
-// function Page(){
-//     return(
-//             <div>
-//             <header>
-//               <nav>
-//                 <img src="./react-logo.png" width="40px"/>
-//                 </nav>
-//             </header>
-//             <h3>
-//                 Reasons I want React as it is an amazing way to be declarative
-//             </h3>
-//             <ol>
-//                 <li>Because it is not difficult</li>
-//                 <li>Less Time to code</li>
-//                 <li>Great skill to have</li>
-//             </ol>
-//             <footer>
-//             <small>Ⓒ 2023 LupaSeal development. All rights reserved.</small>
-//             </footer>
-//             </div>
-//     )
-// }
+function MainContent(){
+    return(
+        <><h3>
+            Reasons I want React as it is an amazing way to be declarative
+        </h3><ol>
+                <li>Because it is not difficult</li>
+                <li>Less Time to code</li>
+                <li>Great skill to have</li>
+            </ol></>
+    )
+}
 
-// ReactDOM.render(<Page />,document.getElementById("root"))
+function Page(){
+    return(
+            <div>
+            <Header />
+            <MainContent />
+            <Footer />
+            </div>
+    )
+}
 
-1. what is a React component?
-reusable code
-2.child without parent
-3.missing div or empty <></>
+ReactDOM.render(<Page />,document.getElementById("root"))
+
